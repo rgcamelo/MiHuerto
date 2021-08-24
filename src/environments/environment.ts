c7e592525dbb1a1e146cba8046d84703e0fa65ad
Rafael Camelo <rafaelgcamelo@gmail.com>
2021-08-24 18:53:04 -0500

    Actualizacion API Firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -10,12 +10,12 @@ export const environment = {
   apiUrl: 'https://huertoapi.herokuapp.com/',
   apiMoon: 'https://www.icalendar37.net/lunar/api/',
   firebaseConfig: {
-    apiKey: "AIzaSyB4KHcAMPJeCAudM-RNQreXGj8mttIXRyk",
-    authDomain: "huertp-e3aa8.firebaseapp.com",
-    projectId: "huertp-e3aa8",
-    storageBucket: "huertp-e3aa8.appspot.com",
-    messagingSenderId: "415122871397",
-    appId: "1:415122871397:web:1b93d49836bf9895c6228c"
+    apiKey: "AIzaSyCyC9eYCJZ9qYoyOjoYOX6Eo84YtO5jyVI",
+    authDomain: "mihuertoapp-a2783.firebaseapp.com",
+    projectId: "mihuertoapp-a2783",
+    storageBucket: "mihuertoapp-a2783.appspot.com",
+    messagingSenderId: "526427126260",
+    appId: "1:526427126260:web:a82c9aa8c384abe2ee61a1"
   }
 };
 
